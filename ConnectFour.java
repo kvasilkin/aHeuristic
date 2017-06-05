480b06a57d0e08881d050ea63272c02ecdbad5ca
kvasilkin <kvasilkin@stuy.edu>
2017-06-05 00:38:03 -0400

    compiles

--- ConnectFour.java
+++ ConnectFour.java
@@ -1,10 +1,10 @@
-	import java.util.*; 
-		import java.io.*; 
+import java.util.*; 
+import java.io.*; 
 public class ConnectFour {
     //Vars
-    private    char [][] board;
-    private boolean player; //true == red false = = black
-    private int numPlayers;
+    private static     char [][] board;
+    private static boolean player; //true == red false = = black
+    private static int numPlayers;
 
     //constructor
 
@@ -15,31 +15,36 @@ public class ConnectFour {
 	for (int r = 0; r < 7; r++){
 	    for (int c = 0; c < 6; c++ ){
 		board[r][c] = 'x';
-}}
+	    }
+	}
 
-}
+    }
 
 
 
- public void ConnectFour(){
+    public void ConnectFour(){
 	board = new char [7][6];
 	numPlayers = 1;
 	player = true; //red}
 	for (int r = 0; r < 7; r++){
 	    for (int c = 0; c < 6; c++ ){
 		board[r][c] = 'x';
-}}
-
+	    }
+	}
+    }
+    
 
 
     //checks if coord is a valid move
 
     public static  boolean isValidMove(int r,int c){
 
-	if (c == 0){return  true;}
-	if (board[r][c] == 'x' && board[r][c - 1] == 'x'){return  true;}
+	if (c == 0){return  true;
+	}
+	if (board[r][c] == 'x' && board[r][c - 1] == 'x'){return  true;
+	}
 	return false;
-}
+    }
     
     //puts down a piece 
     public static  void move(int c){
@@ -49,11 +54,12 @@ public class ConnectFour {
 		    board[c][i] = 'r';
 		}
 		else{
-		    board[c][i] = 'b'
+		    board[c][i] = 'b';
 			}
 	    }
 	}
-	player = !player; }
+	player = !player;
+    }
  
     //evaluates if the board is in a winning state for red
     public static  boolean    isRedWin(){
@@ -64,11 +70,11 @@ public class ConnectFour {
 			return true;
 		    }
 		}
-		    if (c < 3){
+		if (c < 3){
 		    if (board [r][c] =='r' && board [r][c + 1] =='r' && board [r ][c + 2] =='r' && board [r][c + 3] =='r' ){
 			return true;
 		    }
-		    }
+		}
 
 		  
 	    }
@@ -76,18 +82,19 @@ public class ConnectFour {
     
 	for (int r= 0; r < 4; r++){
 	    for (int c = 0; c < 3; c++){
- if (board [r][c] =='r' && board [r + 1][c +1 ] =='r' && board [r + 2][c +2] =='r' && board [r + 3][c +3 ] =='r' ){
-			return true; }
+		if (board [r][c] =='r' && board [r + 1][c +1 ] =='r' && board [r + 2][c +2] =='r' && board [r + 3][c +3 ] =='r' ){
+		    return true; }
 	    }
 	}
 
 	for (int r = 6; r > 7 ; r--){
 	    for (int c = 0; c < 3; c++){
- if (board [r][c] =='r' && board [r - 1][c - 1 ] =='r' && board [r - 2][c - 2] =='r' && board [r - 3][c - 3 ] =='r' ){
-			return true; }
+		if (board [r][c] =='r' && board [r - 1][c - 1 ] =='r' && board [r - 2][c - 2] =='r' && board [r - 3][c - 3 ] =='r' ){
+		    return true; }
 	    }
 	}
-	return false; }
+	return false;
+    }
  
 
 
@@ -98,7 +105,7 @@ public class ConnectFour {
 
 
     //checks if board is in a winnign state for black
-public static  boolean    isBlackWin(){
+    public static  boolean    isBlackWin(){
 	for (int r= 0; r < 7; r++  ){
 	    for (int c = 0; c < 6; c++){
 		if (r < 4){
@@ -106,11 +113,11 @@ public static  boolean    isBlackWin(){
 			return true;
 		    }
 		}
-		    if (c < 3){
+		if (c < 3){
 		    if (board [r][c] =='b' && board [r][c + 1] =='b' && board [r ][c + 2] =='b' && board [r][c + 3] =='b' ){
 			return true;
 		    }
-		    }
+		}
 
 		  
 	    }
@@ -118,15 +125,15 @@ public static  boolean    isBlackWin(){
     
 	for (int r= 0; r < 4; r++){
 	    for (int c = 0; c < 3; c++){
- if (board [r][c] =='b' && board [r + 1][c +1 ] =='b' && board [r + 2][c +2] =='b' && board [r + 3][c +3 ] =='b' ){
-			return true; }
+		if (board [r][c] =='b' && board [r + 1][c +1 ] =='b' && board [r + 2][c +2] =='b' && board [r + 3][c +3 ] =='b' ){
+		    return true; }
 	    }
 	}
 
 	for (int r = 6; r > 7 ; r--){
 	    for (int c = 0; c < 3; c++){
- if (board [r][c] =='b' && board [r - 1][c - 1 ] =='b' && board [r - 2][c - 2] =='b' && board [r - 3][c - 3 ] =='b' ){
-			return true; }
+		if (board [r][c] =='b' && board [r - 1][c - 1 ] =='b' && board [r - 2][c - 2] =='b' && board [r - 3][c - 3 ] =='b' ){
+		    return true; }
 	    }
 	}
 	return false; }
@@ -135,12 +142,14 @@ public static  boolean    isBlackWin(){
 
 
     //prints out char representation of board
-    public String toString{
-String result = ""
-	for (int r = 0; r < 7; r++){
-	    for (int c = 0; c < 6; c++ ){
-		result + board[r][c];
+    public  String toString(){
+	String result = "";
+	    for (int r = 0; r < 7; r++){
+		result += "/n";
+		for (int c = 0; c < 6; c++ ){
+		    result += board[r][c];
+		}
 	    }
-	}
-return result;
+	return result;
     }
+}
