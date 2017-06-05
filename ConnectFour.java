05123e08d3854e354d5cc5e6af2bf42d44e2898e
kvasilkin <kvasilkin@stuy.edu>
2017-06-05 03:22:25 -0400

    move works

--- ConnectFour.java
+++ ConnectFour.java
@@ -2,33 +2,32 @@ import java.util.*;
 import java.io.*; 
 public class ConnectFour {
     //Vars
-    private static     char [][] board;
+    private static     char [][] board = new char [6][7];
     private static boolean player; //true == red false = = black
     private static int numPlayers;
 
     //constructor
 
-    public void ConnectFour(int p){
-	board = new char [7][6];
+    public  ConnectFour(int p){
+	//board = new char [7][6];
 	numPlayers = p;
 	player = true; //red
-	for (int r = 0; r < 7; r++){
-	    for (int c = 0; c < 6; c++ ){
-		board[r][c] = 'x';
+	for (int r = 0; r < 6; r++){
+	    for (int c = 0; c < 7; c++ ){
+		board[r][c] = 'o';
 	    }
 	}
 
     }
 
 
-
-    public void ConnectFour(){
-	board = new char [7][6];
-	numPlayers = 1;
+    public  ConnectFour(){
+	//board = new char [7][6];
+	numPlayers = 2;
 	player = true; //red}
-	for (int r = 0; r < 7; r++){
-	    for (int c = 0; c < 6; c++ ){
-		board[r][c] = 'x';
+	for (int r = 0; r < 6; r++){
+	    for (int c = 0; c < 7; c++ ){
+		board[r][c] = 'o';
 	    }
 	}
     }
@@ -37,30 +36,31 @@ public class ConnectFour {
 
     //checks if coord is a valid move
 
-    public static  boolean isValidMove(int r,int c){
+    public   boolean isValidMove(int r,int c){
 
 	if (c == 0){return  true;
 	}
-	if (board[r][c] == 'x' && board[r][c - 1] == 'x'){return  true;
+	if (board[r][c] == 'o' && board[r][c - 1] == 'o'){return  true;
 	}
 	return false;
     }
     
     //puts down a piece 
-    public static  void move(int c){
-	for(int i = 7; i > -1; i--){
-	    if (board[c][i] == 'x'){
+    public   boolean move(int c){
+	for(int i = 5; i > -1; i--){
+	    if (board[i][c] == 'o'){
 		if (player){
-		    board[c][i] = 'r';
-		}
+		    board[i][c] = 'r';
+		player = !player;
+		return true;}
 		else{
-		    board[c][i] = 'b';
-			}
-	    }
-	}
+		   board[i][c] = 'b';
 	player = !player;
+	return true;	
+	}    	
+	}
     }
- 
+return player; }
     //evaluates if the board is in a winning state for red
     public static  boolean    isRedWin(){
 	for (int r= 0; r < 7; r++  ){
@@ -144,12 +144,32 @@ public class ConnectFour {
     //prints out char representation of board
     public  String toString(){
 	String result = "";
-	    for (int r = 0; r < 7; r++){
-		result += "/n";
-		for (int c = 0; c < 6; c++ ){
-		    result += board[r][c];
+	    for (int r = 0; r < 6; r++){
+		for (int c = 0; c < 7; c++ ){
+		    result +=  " " + board[r][c];
 		}
+		result += '\n';
 	    }
 	return result;
+    }
+
+    public static void main(String[] args) {
+ConnectFour field = new ConnectFour();
+//System.out.println(field.toString());
+
+field.move(3);
+field.move(4);
+field.move(3);
+field.move(5);
+field.move(3);
+field.move(5);
+field.move(3);
+
+System.out.println(field.toString());
+ //System.out.println(Arrays.deepToString(board));
+//field.move(3);
+//System.out.println(field);
+
+
     }
 }
