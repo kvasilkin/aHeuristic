f1544f9e0e287892d4564fe85ecb150e7c3f81fd
lorenzoschneiderman <lschneiderman@stuy.edu>
2017-06-12 01:50:50 -0400

    nother major update

--- ConnectFourTest.java
+++ ConnectFourTest.java
@@ -1,5 +1,6 @@
 // trying minimax
 
+import java.lang.Math;
 public class ConnectFourTest{
     //Vars
     private char [][] board; // to be replaced with static function calls
@@ -7,14 +8,14 @@ public class ConnectFourTest{
     
 
     // class that stores and retrieves connect 4 states
-    public DataSet storage;
+    // public DataSet storage;
 
 
-    public static int[] showMoves(int[][] board){
+    public static int[] showMoves(char[][] board){
 
     	// iterate through the top row to see remaining valid moves
     	
-    	int[] out = int[7];
+    	int[] out = new int[7];
     	int len = 0;
     	for (int col = 0; col < board[0].length; col++){
     		if (board[1][col] == ' '){
@@ -24,9 +25,9 @@ public class ConnectFourTest{
     	}
 
     	// trim out to contain no null values
-        int[] trimmed = int[len];
+        int[] trimmed = new int[len];
         for (int col = 0; col < 7 ; col++){
-            if (out[col] != null){
+            if (out[col] != 0){
                 trimmed[col] = out[col];
             }
         }
@@ -34,7 +35,7 @@ public class ConnectFourTest{
     }
 
     // digs through dataset to find heuristic // implement if time 
-    public float getHeurisiticStat(boolean player){
+    public double getHeurisiticStat(boolean player){
     	
     	// return % of wins from that position
     	// first check if win or loss--
@@ -51,20 +52,20 @@ public class ConnectFourTest{
     }
 
     // finds if winning move
-    public float getHeurisitic(boolean player){
-        if (player){
-            // return +1000 if winning move
-        }
-        else{
-            // return -1000 if enemy winning move
-        }
-    }
+    // public double getHeurisitic(boolean player){
+    //     if (player){
+    //         // return +1000 if winning move
+    //     }
+    //     else{
+    //         // return -1000 if enemy winning move
+    //     }
+    // }
 
 
     // return board with new piece put at col c
-    public static int[][] returnAppliedMove(int[][] board, int c, boolean team){
+    public static char[][] returnAppliedMove(char[][] board, int c, boolean team){
 
-        out = int[7][6];
+        char[][] out = new char[7][6];
         for (int row = 0; row < out.length; row++){
             for (int col = 0; col < out[row].length; col++){
                 out[row][col] = board[row][col];
@@ -73,34 +74,35 @@ public class ConnectFourTest{
         
         // starting from the bottom, check for space and return
         for (int row = 0; row < board.length; row++){
-            if (board[row][col] == ' '){
+            if (board[row][c] == ' '){
                 
                 // if human, place red, else place black
                 if (team){
-                    out[row][col] = 'r';
+                    out[row][c] = 'r';
                     return out;
                 }
                 else{
-                    out[row][col] = 'b';
+                    out[row][c] = 'b';
                     return out;
                 }
             }
 
         }
+        return out;
 
     }
 
-    public int returnAIMove(){
+    public int returnAIMove(char[][] board, int depth){
         
         
-        maxFitnessMove = 3 // default middle
-        maxFitnessSeen = -999
+        int maxFitnessMove = 3; // default middle
+        double maxFitnessSeen = -999.0;
 
         // finds and returns maximum fitness move
-        for (int col : this.showMoves()){
+        for (int col : showMoves(board)){
 
             //                                                team false = computer
-            movefitness = minimaxIteration(returnAIMove(board, col), depth, false);
+            double movefitness = minimaxIteration(returnAppliedMove(board, col, false), depth, false);
             
             if (movefitness > maxFitnessSeen){
                 maxFitnessSeen = movefitness;
@@ -112,36 +114,43 @@ public class ConnectFourTest{
 
     }
 
-    // nec. implement
+    // unnec. implement
     // return true if win, false if not
-    public boolean isWinningMoveSpot(int[][] board, int nextMove, boolean player){
-        for (int row = 0; row < board.length ; row++){
-            if (board[row][col] = ' '){
-                return checkSpotSolution(board, row, col);
-            }
-        }
-        return false;
-    }
+    // public boolean isWinningMoveSpot(char[][] board, int nextMove, boolean player){
+    //     for (int row = 0; row < board.length ; row++){
+    //         if (board[row][nextMove] == ' '){
+    //             return checkSpotSolution(board, row, nextMove);
+    //         }
+    //     }
+    //     return false;
+    // }
 
-    public boolean isWinningMoveWhole(int[][] board, int nextMove, boolean player){
-        for (int row = 0; row < board.length ; row++){
-            if (board[row][col] = ' '){
-                if player{
-                    board[row][col] = 'r';
-                    return isRedWin(board);
-                }
-                else{
-                    board[row][col] = 'b';
-                    return isBlackWin(board);
-                }
 
-            }
+    public boolean isWinningMoveWhole(char[][] board, int nextMove, boolean player){
+        // for (int row = 0; row < board.length ; row++){
+        //     if (board[row][nextMove] == ' '){
+        //         if (player){
+        //             board[row][nextMove] = 'r';
+        //             return isRedWin(returnAppliedMove(board, nextMove, true);
+        //         }
+        //         else{
+        //             board[row][nextMove] = 'b';
+        //             return isBlackWin(returnAppliedMove(board, nextMove, true);
+        //         }
+
+        //     }
+        // }
+
+        if (player){
+            return isRedWin(returnAppliedMove(board, nextMove, true));
         }
-        return false;
+        else{
+            return isBlackWin(returnAppliedMove(board, nextMove, false));
+        }
     }
 
     
-    public float minimaxIteration(ConnectFourTest board, int depth, boolean player){//, float alpha, float beta){
+    public double minimaxIteration(char[][] board, int depth, boolean player){//, float alpha, float beta){
 
 
         // implement
@@ -152,36 +161,120 @@ public class ConnectFourTest{
 
         // if win return 999 if loss return -999
     	
+
+        if (depth == 0){
+            return 0;
+        }
         if (player == false){
-    		bestVal = -999;
+    		double bestVal = -999.0;
 
     		for (int nextMove : showMoves(board)){
     			// value of the node is found in... minimax   (new board pos., lowered depth, other team)
-    			if (isWinningMove(board, nextMove, false)){
+    			if (isWinningMoveWhole(board, nextMove, false)){
                     return 1000;
                 }
                 else{
-                    val = minimaxIteration(returnAppliedMove(board, nextMove, false), depth - 1, true);
-                    bestValue = Math.max(val, bestValue);
+                    double val = minimaxIteration(returnAppliedMove(board, nextMove, false), depth - 1, true);
+                    bestVal = Math.max(val, bestVal);
                 }
             }
-            return bestValue;
+            return bestVal;
     	}
     	
         // if human 
         else{
-    		bestVal = 999;
+    		double bestVal = 999.0;
             for (int nextMove : showMoves(board)){
-    			if (isWinningMove(board, nextMove, true)){
+    			if (isWinningMoveWhole(board, nextMove, true)){
                     return -1000;
                 }
                 else{
-        			val = minimaxIteration(board.returnAppliedMove(nextMove), depth - 1, false);
-        			bestValue = Math.min(val, bestValue);
+        			double val = minimaxIteration(returnAppliedMove(board, nextMove, true), depth - 1, false);
+        			bestVal = Math.min(val, bestVal);
                 }
             }
-            return bestValue;
+            return bestVal;
     	}
 	}
 
+// ---------------------------------------
+    public static  boolean    isRedWin(char[][] board){
+    for (int r= 0; r < 6; r++  ){
+        for (int c = 0; c < 7; c++){
+        if (r < 3){
+            if (board [r][c] =='r' && board [r + 1][c] =='r' && board [r + 2][c] =='r' && board [r + 3][c] =='r' ){
+            return true;
+            }
+        }
+        if (c < 4){
+            if (board [r][c] =='r' && board [r][c + 1] =='r' && board [r ][c + 2] =='r' && board [r][c + 3] =='r' ){
+            return true;
+            }
+        }
+
+          
+        }
+    }
+    
+    for (int r = 0; r < 3; r++){
+        for (int c = 0; c < 4; c++){
+        if (board [r][c] =='r' && board [r + 1][c +1 ] =='r' && board [r + 2][c +2] =='r' && board [r + 3][c +3 ] =='r' ){
+            return true; }
+        }
+    }
+
+    for (int r = 0; r < 3 ; r++){
+        for (int c = 6; c > 2; c--){
+        if (board [r][c] =='r' && board [r + 1][c - 1 ] =='r' && board [r + 2][c - 2] =='r' && board [r + 3][c - 3 ] =='r' ){
+            return true; }
+        }
+    }
+    return false;
+    }
+ 
+
+
+
+
+
+
+
+
+    //checks if board is in a winnign state for black
+ public static  boolean    isBlackWin(char[][] board){
+    for (int r= 0; r < 6; r++  ){
+        for (int c = 0; c < 7; c++){
+        if (r < 3){
+            if (board [r][c] =='b' && board [r + 1][c] =='b' && board [r + 2][c] =='b' && board [r + 3][c] =='b' ){
+            return true;
+            }
+        }
+        if (c < 4){
+            if (board [r][c] =='b' && board [r][c + 1] =='b' && board [r ][c + 2] =='b' && board [r][c + 3] =='b' ){
+            return true;
+            }
+        }
+
+          
+        }
+    }
+    
+    for (int r = 0; r < 3; r++){
+        for (int c = 0; c < 4; c++){
+        if (board [r][c] =='b' && board [r + 1][c +1 ] =='b' && board [r + 2][c +2] =='b' && board [r + 3][c +3 ] =='b' ){
+            return true; }
+        }
+    }
+
+    for (int r = 0; r < 3 ; r++){
+        for (int c = 6; c > 2; c--){
+        if (board [r][c] =='b' && board [r + 1][c - 1 ] =='b' && board [r + 2][c - 2] =='b' && board [r + 3][c - 3 ] =='b' ){
+            return true; }
+        }
+    }
+    return false;
+    }
+
 }
+
+
